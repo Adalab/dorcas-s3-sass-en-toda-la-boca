a4450e773c8ea7b0c6371b56dc4f186135f752a4
sinsit1 <alexandra8989@gmail.com>
2018-07-29 13:18:41 +0200

    refactor colapsibles and createtarget

--- proyecto-antiguo/js/desplegables.js
+++ proyecto-antiguo/js/desplegables.js
@@ -1,16 +1,16 @@
 'use strict';
 
 //mete en la variable todos las clases que existen collapsible__container
-var collapsibleContainer = document.querySelectorAll('.collapsible__container');
+const collapsibleContainer = document.querySelectorAll('.collapsible__container');
 //mete en la variable todos las clases que existen collapsible__label
-var collapsibleLabel = document.querySelectorAll('.collapsible__label');
+const collapsibleLabel = document.querySelectorAll('.collapsible__label');
 
 
-function updateCollapsibles(e) {
+const updateCollapsibles = (e) => {
 
   //se le pasa el evento del click en el collapsible que se haya clicado
   // y mete en la variable el padre de ese collapsible
-  var padreCollapsibleLabel = e.currentTarget.parentElement;
+  const padreCollapsibleLabel = e.currentTarget.parentElement;
 
 
   //si el padre del collapsible que se ha clicado tiene la clase
@@ -25,21 +25,21 @@ function updateCollapsibles(e) {
     padreCollapsibleLabel.classList.add('collapsible--visible');
   }
 
-  var dataCol = padreCollapsibleLabel.getAttribute('data-valor');
-  var dataIcon = document.querySelectorAll('.icon-collapsible');
+  const dataCol = padreCollapsibleLabel.getAttribute('data-valor');
+  const dataIcon = document.querySelectorAll('.icon-collapsible');
 
-  for(i = 0; i < dataIcon.length; i++){
-    if (dataIcon[i].getAttribute('data-valor') == dataCol){
-      if (dataIcon[i].classList.contains('fa-chevron-down')){
-        console.log("arriba");
-         dataIcon[i].classList.remove('fa-chevron-down');
-          dataIcon[i].classList.add('fa-chevron-up');
-         
+  for (let i = 0; i < dataIcon.length; i++) {
+    if (dataIcon[i].getAttribute('data-valor') == dataCol) {
+      if (dataIcon[i].classList.contains('fa-chevron-down')) {
+
+        dataIcon[i].classList.remove('fa-chevron-down');
+        dataIcon[i].classList.add('fa-chevron-up');
+
       } else if (dataIcon[i].classList.contains('fa-chevron-up')) {
         console.log("abajo");
         dataIcon[i].classList.remove('fa-chevron-up');
         dataIcon[i].classList.add('fa-chevron-down');
-        
+
       }
     }
   }
@@ -47,19 +47,19 @@ function updateCollapsibles(e) {
   console.log("data", dataCol);
 
 
-}
+};
 
-//funcion que va elimininando la clase collapsible--visible 
-function closeAllCollapsibles() {
-  for (var i = 0; i < collapsibleContainer.length; i++) {
+//funcion que va elimininando la clase collapsible--visible
+const closeAllCollapsibles = () => {
+  for (let i = 0; i < collapsibleContainer.length; i++) {
     collapsibleContainer[i].classList.remove('collapsible--visible');
   }
-}
+};
 
 
 //recorre el array de clases de collapsible__label y cada vez que
 // se clica en un collapsible llama a la funcion updateCollapsibles
-for (var i = 0; i < collapsibleContainer.length; i++) {
+for (let i = 0; i < collapsibleContainer.length; i++) {
 
   collapsibleLabel[i].addEventListener('click', updateCollapsibles);
 }
