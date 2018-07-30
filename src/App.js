0446b355072b28680c8cabfaedd6c9fabba2c95e
G3MM4 <gemmvaughan@gmail.com>
2018-07-30 20:23:28 +0200

    add fetch function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,32 @@ import Main from './Main';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        skills: []
+      }
+    this.catchFetch()
+  }
+
+  catchFetch () {
+    fetch('https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/skills.json')
+      .then(function (response) {
+        return response.json();
+      }    
+    )
+    .then(function (json){
+      this.setState({
+        skills: json.skills
+        
+      }
+        )
+      console.log(this.setState);
+    }
+   )
+  }
+ 
+
   render() {
     return (
       <div className="page__wrapper">
