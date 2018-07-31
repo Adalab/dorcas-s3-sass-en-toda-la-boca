a89ff5901acee7dec3098f91b3f5cf476cbb0a97
sinsit1 <alexandra8989@gmail.com>
2018-07-31 21:04:07 +0200

    finish link object preview

--- src/Preview.js
+++ src/Preview.js
@@ -1,13 +1,21 @@
 import React from 'react';
-//import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 
 const paletteClasses= {
     '1': "primary-palette",
     '2': "secondary-palette",
-    '3': "tertiary-palette"
+    '3': "tertiary-palette",
+    '4': "hearts-palette"
   }
   
+const typographies ={
+    '1': 'font__style-font1',
+    '2': 'font__style-font2',
+    '3': 'font__style-font3',
+    '4': 'font__style-font4'
+}
+
 class Preview extends React.Component {
     constructor(props) {
         super(props);
@@ -21,7 +29,7 @@ class Preview extends React.Component {
 
     render() {
         console.log(this.props);
-        const { name, job, palette } = this.props.data;
+        const { palette, typography ,name, job, phone, email, linkedin, github, skills} = this.props.data;
         return (
             <div className="main__section-card">
                 <section className="section--top">
@@ -33,30 +41,30 @@ class Preview extends React.Component {
                             </button>
                         </div>
                         <div className={`section--top__card ${paletteClasses[palette]}`}>
-                            <span className="card__title font__style-font1 localcard--name" id="card--name">
+                            <span className={`card__title ${typographies[typography]} localcard--name`} id="card--name">
                                 {name}
                             </span>
-                            <span className="card__subtitle" id="card--job">
+                            <span className={`card__subtitle ${typographies[typography]}`} id="card--job">
                                 {job}
                             </span>
                             <img className="photo" src="https://picsum.photos/240/200" alt="foto" />
                             <ul className="card__media">
-                                <a className="localcard--phone" id="tlf" href="" target="_blank">
+                                <a className="localcard--phone" id="tlf" href={`tel:${phone}`}   target="_blank">
                                     <li className="card__media--icon">
                                         <i className="fas fa-mobile-alt icon-card-color"></i>
                                     </li>
                                 </a>
-                                <a className="localcard--email" id="email" href="" target="_blank">
+                                <a className="localcard--email" id="email" href={`mailto:${email}`}  target="_blank">
                                     <li className="card__media--icon">
                                         <i className="far fa-envelope icon-card-color"></i>
                                     </li>
                                 </a>
-                                <a className="localcard--linkedin" id="linkedin" href="" target="_blank">
+                                <a className="localcard--linkedin" id="linkedin" href={` https://es.linkedin.com/in/: ${linkedin}`}   target="_blank">
                                     <li className="card__media--icon">
                                         <i className="fab fa-linkedin-in icon-card-color"></i>
                                     </li>
                                 </a>
-                                <a className="localcard--github" id="github" href="" target="_blank">
+                                <a className="localcard--github" id="github" href={` https://github.com/: ${github}`} target="_blank">
                                     <li className="card__media--icon">
                                         <i className="fab fa-github-alt icon-card-color"></i>
                                     </li>
@@ -67,13 +75,13 @@ class Preview extends React.Component {
 
                             <ul className="card__skills">
                                 <li className="card__skills--icon">
-                                    <span className="icon__text">HTML</span>
+                                    <span className={`icon__text ${typographies[typography]} `}>{skills[0]}</span>
                                 </li>
                                 <li className="card__skills--icon">
-                                    <span className="icon__text">CSS</span>
+                                    <span className={`icon__text ${typographies[typography]} `}>{skills[1]}</span>
                                 </li>
                                 <li className="card__skills--icon">
-                                    <span className="icon__text">GULP</span>
+                                    <span className={`icon__text ${typographies[typography]} `}>{skills[2]}</span>
                                 </li>
                             </ul>
                         </div>
