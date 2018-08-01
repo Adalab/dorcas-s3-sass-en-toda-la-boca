b73dbdb31c915fade6497cc8c35efbba1546aa75
Mariví <marivirh82@gmail.com>
2018-08-01 12:22:09 +0200

    add states on form inputs

--- src/Rellena.js
+++ src/Rellena.js
@@ -5,12 +5,24 @@ class Rellena extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      image: ''
+      image: '',
+      name: '',
+      job: '',
+      email: '',
+      tel: '',
+      linkedin: '',
+      github: ''
     }
     this.fileInput = React.createRef();
     this.handleInputImageButton = this.handleInputImageButton.bind(this);
     this.handleClickInput = this.handleClickInput.bind(this);
     this.handleInputFile = this.handleInputFile.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleJobChange = this.handleJobChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleTelChange = this.handleTelChange.bind(this);
+    this.handleLinkedinChange = this.handleLinkedinChange.bind(this);
+    this.handleGithubChange = this.handleGithubChange.bind(this);
   }
 
   handleInputImageButton () {
@@ -34,6 +46,43 @@ class Rellena extends React.Component {
     fr.readAsDataURL(event.target.files[0]);
 }
 
+  handleNameChange (event) {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleJobChange (event) {
+    this.setState({
+      job: event.target.value
+    })
+  }
+
+  handleEmailChange (event) {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  handleTelChange (event) {
+    this.setState({
+      tel: event.target.value
+    })
+  }
+
+  handleLinkedinChange (event) {
+    this.setState({
+      linkedin: event.target.value
+    })
+  }
+
+  handleGithubChange (event) {
+    this.setState({
+      github: event.target.value
+    })
+  }
+
+
     render() {
         return (
             <Colapsable>
@@ -52,12 +101,11 @@ class Rellena extends React.Component {
               <div className="collapsible__content">
   
                 <label className="label--stuffed" htmlFor="name">Nombre completo</label>
-                <input className="input--stuffed input--text local--name" data-elementos="card--name" placeholder="Ej: Sally Hill" id="complete--name"
-                  type="text" name="name" value="" />
+                <input className="input--stuffed input--text local--name" data-elementos="card--name" placeholder="Ej: Sally Hill" id="complete--name" type="text" name="name" value={this.state.name} onChange={this.handleNameChange} />
   
                 <label className="label--stuffed" htmlFor="job">Puesto</label>
                 <input className="input--stuffed input--text local--job" data-elementos="card--job" placeholder="Ej: Front-end unicorn" id="job"
-                  type="text" name="job" value=""/>
+                  type="text" name="job" value={this.state.job} onChange={this.handleJobChange}/>
   
                 <label className="label--stuffed" htmlFor="img--profile">Imagen de perfil</label>
                 <div className="div--stuffed__img">
@@ -70,19 +118,18 @@ class Rellena extends React.Component {
   
                 <label className="label--stuffed" htmlFor="email">Email</label>
                 <input className="input--stuffed input--href local--email" data-href="email" data-type="email" placeholder="Ej: sally-hill@gmail.com" id="email"
-                  type="email" name="email" value="" />
+                  type="email" name="email" value={this.state.email} onChange={this.handleEmailChange} />
   
                 <label className="label--stuffed" htmlFor="phone">Teléfono</label>
-                <input className="input--stuffed input--href local--phone" data-href="tlf" data-type="tlf" placeholder="Ej:555-555-555" id="tlf" type="tel" name="phone"
-                  value="" />
+                <input className="input--stuffed input--href local--phone" data-href="tlf" data-type="tlf" placeholder="Ej:555-555-555" id="tlf" type="tel" name="phone" value={this.state.tel} onChange={this.handleTelChange}/>
   
                 <label className="label--stuffed" htmlFor="linkedin">Linkedin</label>
                 <input className="input--stuffed input--href local--linkedin" data-href="linkedin" data-type="linkedin" placeholder="Ej: sally-hill"
-                  id="lkd" type="text" name="linkedin" value="" />
+                  id="lkd" type="text" name="linkedin" value={this.state.linkedin} onChange={this.handleLinkedinChange} />
   
                 <label className="label--stuffed" htmlFor="github">Github</label>
                 <input className="input--stuffed input--href local--github" data-href="github" data-type="github" placeholder="Ej: sally-hill" id="gith" type="text"
-                  name="github" value="" />
+                  name="github" value={this.state.github} onChange={this.handleGithubChange} />
   
                 <label className="label--stuffed" htmlFor="skills1">Habilidades (máximo 3)</label>
   
